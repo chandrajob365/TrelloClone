641d55de06ed892f4a2095a0fbde5f3d2b1b50fe
Manish <chandrajob365@gmail.com>
2017-09-26 07:24:58 +0530

    Add app, board , header and dialog container

---
+++ app/components/BoardContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class BoardContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+}
+export default BoardContainer

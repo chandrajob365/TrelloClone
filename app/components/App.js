11baa644f61f3f3bcaceb4cab0e621f73307c571
Manish <chandrajob365@gmail.com>
2017-09-26 17:47:56 +0530

    [App.js] Add App.js contains BoardPannel

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BoardPannel from './BoardPannel'
+
+class App extends React.Component {
+  render (props) {
+    return (
+      <div>
+        <BoardPannel />
+      </div>
+    )
+  }
+}
+
+export default App

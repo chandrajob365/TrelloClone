9a8378f9a437f88c71516eefd3d8fb58a21b3e6d
Manish <chandrajob365@gmail.com>
2017-10-04 01:27:03 +0530

    Add new tasks to check wrap feature for flex box

--- data.js
+++ data.js
@@ -3,209 +3,62 @@ const data = {
     '1BuyFruits': {
       boardId: '1BuyFruits',
       boardName: 'BuyFruits',
-      taskList: ['l1', 'l2'],
+      taskList: ['1l1', '2l2', '3l3', '4l4', '5l5', '6l6'],
       desc: ''
     },
-    '2CompleteBoard': {
-      boardId: '2CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
+    '2CompleteApp': {
+      boardId: '2CompleteApp',
+      boardName: 'CompleteApp',
+      taskList: ['1l1'],
       desc: ''
-    },
-    '3CompleteBoard': {
-      boardId: '3CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '4CompleteBoard': {
-      boardId: '4CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '5CompleteBoard': {
-      boardId: '5CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '6CompleteBoard': {
-      boardId: '6CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '7CompleteBoard': {
-      boardId: '7CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '8CompleteBoard': {
-      boardId: '8CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '9CompleteBoard': {
-      boardId: '9CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '10CompleteBoard': {
-      boardId: '10CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '11CompleteBoard': {
-      boardId: '11CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '12CompleteBoard': {
-      boardId: '12CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '13CompleteBoard': {
-      boardId: '13CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '14CompleteBoard': {
-      boardId: '14CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '15CompleteBoard': {
-      boardId: '15CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '16CompleteBoard': {
-      boardId: '16CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '17CompleteBoard': {
-      boardId: '17CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '18BuyFruits': {
-      boardId: '18BuyFruits',
-      boardName: 'BuyFruits',
-      taskList: ['l1', 'l2'],
-      desc: ''
-    },
-    '19CompleteBoard': {
-      boardId: '19CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '19CompleteBoard': {
-      boardId: '19CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '20CompleteBoard': {
-      boardId: '20CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '21CompleteBoard': {
-      boardId: '21CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '22CompleteBoard': {
-      boardId: '22CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '23CompleteBoard': {
-      boardId: '23CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '24CompleteBoard': {
-      boardId: '24CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '25CompleteBoard': {
-      boardId: '25CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '26CompleteBoard': {
-      boardId: '26CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '27CompleteBoard': {
-      boardId: '27CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '28CompleteBoard': {
-      boardId: '28CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '29CompleteBoard': {
-      boardId: '29CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '30CompleteBoard': {
-      boardId: '30CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '31CompleteBoard': {
-      boardId: '31CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '32CompleteBoard': {
-      boardId: '32CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
-    '33CompleteBoard': {
-      boardId: '33CompleteBoard',
-      boardName: 'CompleteBoard',
-      taskList: [],
-      desc: ''
-    },
+    }
   },
-  seqNo: 34
+  tasks: {
+    '1l1': {
+      taskId: '1l1',
+      taskName: 'l1',
+      taskDesc: 'Test task 1',
+      taskDueDate: '2017-10-08',
+      taskCreationDate: '2017-10-03'
+    },
+    '2l2': {
+      taskId: '2l2',
+      taskName: 'l2',
+      taskDesc: 'Test task 2',
+      taskDueDate: '2017-10-08',
+      taskCreationDate: '2017-10-03'
+    },
+    '3l3': {
+      taskId: '3l3',
+      taskName: 'l3',
+      taskDesc: 'Test task 3',
+      taskDueDate: '2017-10-08',
+      taskCreationDate: '2017-10-03'
+    },
+    '4l4': {
+      taskId: '4l4',
+      taskName: 'l4',
+      taskDesc: 'Test task 4',
+      taskDueDate: '2017-10-08',
+      taskCreationDate: '2017-10-03'
+    },
+    '5l5': {
+      taskId: '5l5',
+      taskName: 'l5',
+      taskDesc: 'Test task 5',
+      taskDueDate: '2017-10-08',
+      taskCreationDate: '2017-10-03'
+    },
+    '6l6': {
+      taskId: '6l6',
+      taskName: 'l6',
+      taskDesc: 'Test task 6',
+      taskDueDate: '2017-10-08',
+      taskCreationDate: '2017-10-03'
+    }
+  },
+  currentBoardIndex: 1,
+  currentTaskIndex: 2
 }
 
 export default data

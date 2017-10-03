5301b838799496363e18ca6ae19c46f8ceb3f3f3
Manish <chandrajob365@gmail.com>
2017-10-03 23:22:23 +0530

    [BoardPannel.js] Move delete , add , update method to app.js

--- app/components/BoardPannel.js
+++ app/components/BoardPannel.js
@@ -1,87 +1,16 @@
 import React from 'react'
 import BoardList from './BoardList'
-import data from '../../data.js'
 
 class BoardPannel extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      boards: data.boards,
-      currentBoardIndex: data.currentBoardIndex
-    }
-    this.addBoard = this.addBoard.bind(this)
-    this.updateBoardName = this.updateBoardName.bind(this)
-    this.displayTaskList = this.displayTaskList.bind(this)
-    this.deleteBoard = this.deleteBoard.bind(this)
-  }
-
-  addBoard (boardName) {
-    console.log('<BoardPannel addBoard > boardName = ', boardName)
-    let boardId = this.state.currentBoardIndex + boardName
-    this.setState(
-      {
-        ...this.state,
-        boards: {
-          ...this.state.boards,
-          [boardId]: {
-            boardId: boardId,
-            boardName: boardName,
-            taskList: [],
-            desc: ''
-          }
-        },
-        currentBoardIndex: ++this.state.currentBoardIndex
-      }
-    )
-  }
-
-  updateBoardName (boardName, boardId) {
-    console.log('updatedName = ', boardName, ' id = ', boardId)
-    this.setState(
-      {
-        ...this.state,
-        boards: {
-          ...this.state.boards,
-          [boardId]: {
-            boardId: boardId,
-            boardName: boardName,
-            taskList: [],
-            desc: ''
-          }
-        }
-      }
-    )
-  }
-
-  deleteBoard (boardId) {
-    console.log('<BoardPannel, deleteBoard> boardId = ', boardId)
-    let boardsCopy = Object.assign({}, this.state.boards)
-    console.log('boardsCopy = ', boardsCopy)
-    for (let boardKey in boardsCopy) {
-      if (boardKey === boardId) {
-        delete boardsCopy[boardId]
-        break
-      }
-    }
-    this.setState({
-      boards: boardsCopy,
-      currentBoardIndex: --this.state.currentBoardIndex
-    })
-  }
-
-  displayTaskList (boardId) {
-    console.log('------------ Inside displayTaskList ------------------ boardId = ', boardId)
-  }
-
   render () {
-    console.log('<BoardPannel> this.state = ', this.state)
+    console.log('<BoardPannel> this.props = ', this.props)
     return (
       <div className='nav'>
-        <BoardList boards={this.state.boards}
-          addBoard={this.addBoard}
-          updateBoardName={this.updateBoardName}
-          displayTaskList={this.displayTaskList}
-          deleteBoard={this.deleteBoard} />
+        <BoardList boards={this.props.boards}
+          addBoard={this.props.addBoard}
+          updateBoardName={this.props.updateBoardName}
+          displayTaskList={this.props.displayTaskList}
+          deleteBoard={this.props.deleteBoard} />
       </div>
     )
   }

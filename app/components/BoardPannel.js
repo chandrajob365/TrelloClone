a1eed78f6714651641778486dc91080d89653481
Manish <chandrajob365@gmail.com>
2017-10-03 10:04:35 +0530

    Add addBoard , updateBoard on state

--- app/components/BoardPannel.js
+++ app/components/BoardPannel.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import AddBoard from './AddBoard'
 import BoardList from './BoardList'
 import data from '../../data.js'
 
@@ -7,38 +6,64 @@ class BoardPannel extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      boards: {},
-      currentBoardIndex: 0
+      boards: data.boards,
+      currentBoardIndex: data.currentBoardIndex
     }
-    // this.addBoard = this.addBoard.bind(this)
-    // this.renameBoard = this.renameBoard.bind(this)
+    this.addBoard = this.addBoard.bind(this)
+    this.updateBoardName = this.updateBoardName.bind(this)
+    this.displayTaskList = this.displayTaskList.bind(this)
   }
 
-  // addBoard (boardName) {
-  //   let boardId = this.state.currentBoardIndex + boardName
-  //   this.setState(
-  //     {
-  //       ...this.state,
-  //       boards: {
-  //         ...this.state.boards,
-  //         [boardId]: {
-  //           boardId: boardId,
-  //           boardName: boardName
-  //         }
-  //       },
-  //       currentBoardIndex: ++this.state.currentBoardIndex
-  //     }
-  //   )
-  // }
-  //
-  // renameBoard (boardName) {
-  //
-  // }
+  addBoard (boardName) {
+    console.log('<BoardPannel addBoard > boardName = ', boardName)
+    let boardId = this.state.currentBoardIndex + boardName
+    this.setState(
+      {
+        ...this.state,
+        boards: {
+          ...this.state.boards,
+          [boardId]: {
+            boardId: boardId,
+            boardName: boardName,
+            taskList: [],
+            desc: ''
+          }
+        },
+        currentBoardIndex: ++this.state.currentBoardIndex
+      }
+    )
+  }
 
-  render (props) {
+  updateBoardName (boardName, boardId) {
+    console.log('updatedName = ', boardName, ' id = ', boardId)
+    this.setState(
+      {
+        ...this.state,
+        boards: {
+          ...this.state.boards,
+          [boardId]: {
+            boardId: boardId,
+            boardName: boardName,
+            taskList: [],
+            desc: ''
+          }
+        }
+      }
+    )
+  }
+
+  displayTaskList (boardId) {
+    console.log('------------ Inside displayTaskList ------------------ boardId = ', boardId)
+  }
+
+  render () {
+    console.log('<BoardPannel> this.state = ', this.state)
     return (
       <div className='nav'>
-        <BoardList data={data} />
+        <BoardList boards={this.state.boards}
+          addBoard={this.addBoard}
+          updateBoardName={this.updateBoardName}
+          displayTaskList={this.displayTaskList} />
       </div>
     )
   }

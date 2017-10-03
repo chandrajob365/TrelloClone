b27f8c8615615bbac11cb01e588a6a34b1ae4b26
Manish <chandrajob365@gmail.com>
2017-10-03 10:03:16 +0530

    Add rename functionallity for BoardList

--- app/components/BoardList.js
+++ app/components/BoardList.js
@@ -2,19 +2,47 @@ import React from 'react'
 import Board from './Board'
 
 class BoardList extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      boardName: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState(
+      {boardName: e.target.value}
+    )
+  }
+
+  handleCreate (e) {
+    this.props.addBoard(this.state.boardName)
+    this.setState({boardName: ''})
+    this.refs.boardName.focus()
+  }
+
   render (props) {
-    console.log('this.props = ', this.props.data.boards)
-    console.log('Object keys = ', Object.keys(this.props.data.boards))
-    let boards = this.props.data.boards
+    console.log('<BoardList> this.props.boards = ', this.props.boards)
+    // console.log('Object keys = ', Object.keys(this.props.data.boards))
+    let boards = this.props.boards
     let rows = []
     for (let board in boards) {
       rows.push(
-        <Board key={boards[board].boardId} boardName={boards[board].boardName} />
+        <Board key={boards[board].boardId}
+          board={boards[board]}
+          updateBoardName={this.props.updateBoardName}
+          displayTaskList={this.props.displayTaskList} />
       )
     }
     return (
       <div>
         {rows}
+        <div className='nav-item'>
+          <input type='text' ref='boardName' value={this.state.boardName} placeholder='Add new Board' onChange={this.handleChange} />
+          <input type='button' value='Create' onClick={this.handleCreate} />
+        </div>
       </div>
     )
   }

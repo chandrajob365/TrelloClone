018dbce3cac0c572d4bb132e59ca39f024638b83
Manish <chandrajob365@gmail.com>
2017-10-04 02:06:32 +0530

    Add footer component and related css class

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {render} from 'react-dom'
 import App from './app/components/App'
 import HeaderPannel from './app/components/HeaderPannel'
+import Footer from './app/components/Footer'
 // import style from './app/styles/main.css'
 
 const Root = () => (
   <div className='body'>
     <HeaderPannel />
     <App />
+    <Footer />
   </div>
 )
 

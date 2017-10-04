610c210b7830f4b4a3fa8896018f41d2a6fa4065
Manish <chandrajob365@gmail.com>
2017-10-04 11:34:44 +0530

    [TaskPannel.js] Add create Task functionality

--- app/components/TaskPannel.js
+++ app/components/TaskPannel.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import Task from './Task'
 class TaskPannel extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({value: e.target.value})
+  }
+
+  handleCreate (e) {
+    if (this.state.value.length > 0) {
+      this.props.createTask(this.state.value)
+    }
+  }
+
   render (props) {
+    console.log('<TaskPannel, render >, props = ', this.props)
     let rows = []
     this.props.activeBoardTasks.forEach(task => {
+      console.log('<TaskPannel, render > task = ', task)
       rows.push(
         <Task key={task.taskId}
           task={task} />
@@ -12,6 +33,10 @@ class TaskPannel extends React.Component {
     return (
       <div className='task-pannel-container'>
         {rows}
+        <div className='create-task'>
+          <input type='text' ref='taskName' placeholder='Create new Task' onChange={this.handleChange} />
+          <input type='button' value='Create' onClick={this.handleCreate}/>
+        </div>
       </div>
     )
   }

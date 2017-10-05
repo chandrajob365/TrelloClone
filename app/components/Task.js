ff87c134a029fd0a10759d2e2e9196a71a237631
Manish <chandrajob365@gmail.com>
2017-10-05 12:26:16 +0530

    Add Card component in Task

--- app/components/Task.js
+++ app/components/Task.js
@@ -1,19 +1,37 @@
 import React from 'react'
+import Card from './Card'
+import CreateCard from './CreateItem'
 
 class Task extends React.Component {
+  constructor (props) {
+    super(props)
+    this.createCard = this.createCard.bind(this)
+  }
+
+  createCard (cardName) {
+    this.props.createCard(cardName, this.props.task.taskId)
+  }
+
   render (props) {
     console.log('<Task.js render> props = ', this.props)
+    let cards = []
+    this.props.task.cards.forEach(cardId => {
+      cards.push(
+        <Card key={cardId} card={this.props.cards[cardId]} />
+      )
+    })
     return (
       <div className='task-item'>
         <div className='task-header'>{this.props.task.taskName}</div>
 
         <div className='task-content'>
-          <span> Card1 </span>
-          <span> Card2 </span>
+          {cards}
         </div>
 
         <div className='task-footer'>
-          <span> Add new Card </span>
+          <CreateCard
+            create={this.createCard}
+            placeholder={'Create new Card'} />
         </div>
       </div>
     )

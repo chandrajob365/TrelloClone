a79d543e3b07148bbc91b3b09da5f5247eb9c606
Manish <chandrajob365@gmail.com>
2017-10-10 08:58:02 +0530

    Add provider , store and rootReducer

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import {render} from 'react-dom'
-import App from './app/components/App'
+import AppContainer from './app/container/AppContainer'
 import HeaderPannel from './app/components/HeaderPannel'
 import Footer from './app/components/Footer'
 import {createStore} from 'redux'
 import AppReducer from './app/reducers/AppReducer'
 import {Provider} from 'react-redux'
 
-const Root = () => (
-  <Provider store={createStore(AppReducer)} >
-    <div>
-      <HeaderPannel />
-      <App />
-      <Footer />
-    </div>
-  </Provider>
-)
+const Root = () => {
+  return (
+    <Provider store={createStore(AppReducer)} >
+      <div>
+        <HeaderPannel />
+        <AppContainer />
+        <Footer />
+      </div>
+    </Provider>
+  )
+}
 
 render(
   <Root />,

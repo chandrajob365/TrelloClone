18f3bf420e38b73ee01f8f5d4423a8689b884ebe
Manish <chandrajob365@gmail.com>
2017-10-11 03:12:28 +0530

    [AppReducer] Use combine reducer and custom reducer
    Combine reducer - to use more than one reducers
    
    Custom reducer - TO use/ update common state slices used accross multiple reducers

--- app/reducers/AppReducer.js
+++ app/reducers/AppReducer.js
@@ -1,9 +1,31 @@
 import { combineReducers } from 'redux'
 import BoardReducer from './BoardReducer'
+import TaskReducer from './TaskReducer'
+import createTaskReducer from './CreateTaskReducer'
 
-const AppReducer = combineReducers({
-  boards: BoardReducer
+const combinedReducer = combineReducers({
+  boards: BoardReducer,
+  tasks: TaskReducer
 })
-// export default BoardReducer
+
+const secondReducer = function (state, action) {
+  console.log('<SecondReducer> state = ', state, '  action = ', action)
+  let finalState = null
+  switch (action.type) {
+    case 'CREATE_TASK': finalState = createTaskReducer(state.boards, state.tasks, action)
+      console.log('<SecondReducer>CREATE_TASK case finalState = ', finalState)
+      return finalState
+    default: return state
+  }
+}
+
+const AppReducer = (state, action) => {
+  console.log('<AppReducer> state = ', state, ' action = ', action)
+  const intermediateState = combinedReducer(state, action)
+  console.log('<AppReducer> intermediateState = ', intermediateState)
+  const finalState = secondReducer(intermediateState, action)
+  console.log('<AppReducer> finalState = ', finalState)
+  return finalState
+}
 
 export default AppReducer

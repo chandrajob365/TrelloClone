aa2b8e9011c97e5d1f99512ce7ebdfafc1a35177
Manish <chandrajob365@gmail.com>
2017-10-18 16:06:22 +0530

    Remove storeCreation and Root component creation to seprate files

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import AppContainer from './app/container/AppContainer'
-import HeaderPannel from './app/components/HeaderPannel'
-import Footer from './app/components/Footer'
-import {createStore} from 'redux'
-import AppReducer from './app/reducers/AppReducer'
-import {Provider} from 'react-redux'
-
-let state = {}
-const Root = () => {
-  return (
-    <Provider store={createStore(AppReducer, state)} >
-      <div>
-        <HeaderPannel />
-        <AppContainer />
-        <Footer />
-      </div>
-    </Provider>
-  )
-}
+import Root from './app/react_reduxComponent/Root'
 
 render(
   <Root />,
